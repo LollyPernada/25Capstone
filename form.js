fe94f1948c504cf96c8f06760470c1d6c26ebcbc
Lolly Pernada <119377434+LollyPernada@users.noreply.github.com>
2023-10-09 10:16:49 +0700

    Update form.js

--- form.js
+++ form.js
@@ -1,16 +1,16 @@
 // Fungsi untuk pendaftaran pengguna
 function registerUser() {
     // Mengambil data dari form pendaftaran
-    var newUsername = document.getElementById("newUsername").value;
-    var newEmail = document.getElementById("newEmail").value;
-    var newPassword = document.getElementById("newPassword").value;
+    const newUsername = document.getElementById("newUsername").value;
+    const newEmail = document.getElementById("newEmail").value;
+    const newPassword = document.getElementById("newPassword").value;
 
     // Validasi input
     if (newUsername === "" || newEmail === "" || newPassword === "") {
         alert("Harap isi semua kolom input.");
     } else {
         // Simpan data pengguna ke localStorage (simpanan sementara, seharusnya menggunakan server atau database)
-        var userData = {
+        const userData = {
             username: newUsername,
             email: newEmail,
             password: newPassword
@@ -24,18 +24,18 @@ function registerUser() {
 }
 
 // Fungsi untuk login pengguna
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+document.getElementById("loginForm").addEventListener("submit", (event) => {
     event.preventDefault();
-    var inputUsername = document.getElementById("username").value;
-    var inputPassword = document.getElementById("password").value;
+    const inputUsername = document.getElementById("username").value;
+    const inputPassword = document.getElementById("password").value;
 
     // Mengambil data pengguna yang sudah terdaftar dari localStorage
-    var userData = JSON.parse(localStorage.getItem("userData"));
+    const userData = JSON.parse(localStorage.getItem("userData"));
 
     if (userData && inputUsername === userData.username && inputPassword === userData.password) {
         // Jika kredensial sesuai, arahkan ke halaman beranda setelah login berhasil
         alert("Login berhasil!");
-        window.location.href = "main.html"; // Ganti "info.html" dengan halaman beranda 
+        window.location.href = "index.html"; // Ganti "info.html" dengan halaman beranda Anda
     } else {
         // Jika kredensial tidak sesuai, tampilkan pesan kesalahan
         alert("Username atau Password yang Anda masukkan salah. Silakan periksa kembali input Anda.");
